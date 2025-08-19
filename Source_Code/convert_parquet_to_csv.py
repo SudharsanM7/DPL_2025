0bc97df88504d59e26a1187c2a2c8a5ceb7a338e
SudharsanM7 <winnings.yt@gmail.com>
2025-08-19 22:17:23 +0530

    Update and rename Hello.txt to convert_parquet_to_csv.py

---
+++ Source_Code/convert_parquet_to_csv.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import fastparquet
+parquet_path = "/content/drive/MyDrive/DPL 2025/artifacts/<file_name>"
+csv_path = "/content/drive/MyDrive/DPL 2025/artifacts/<file_name>"
+df = pd.read_parquet(parquet_path, engine='fastparquet')
+df.to_csv(csv_path, index=False)
+print(f"Saved CSV to {csv_path}")
+
